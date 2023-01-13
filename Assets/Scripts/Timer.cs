a86122a1809b478b6d3a7d25a1395580e585675c
movafegh <mo.movafegh@gmail.com>
2023-01-13 17:06:45 +0330

    UI Fixed and auto answer detect added

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using System.Linq;
 using System;
 using TMPro;
+using UnityEngine.UI;
+
 public class Timer : MonoBehaviour
 {
 
@@ -14,6 +16,10 @@ public class Timer : MonoBehaviour
     public int line;
      
      [SerializeField]TextMeshProUGUI getMorse;
+     [SerializeField]TextMeshProUGUI getWord;
+     [SerializeField] float spaceDelay = 10f;
+     [SerializeField] Button spaceButton; 
+     ColorBlock theColor;
 
     public string[] alphabet = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N",
     "O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"};
@@ -26,7 +32,13 @@ public class Timer : MonoBehaviour
     "-----",".----","..---","...--","....-",".....","-....","--...","---..","----."};
 
     public string currentMorse;
+    void start()
+    {
+        spaceButton = GetComponent<Button>();
+        theColor = GetComponent<Button>().colors;
+    }
 
+    
     
     void Update()
     {
@@ -41,14 +53,30 @@ public class Timer : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             timerValue += Time.deltaTime;
-
+            theColor.normalColor = new Color32(133,133,133,133);
+            theColor.highlightedColor = new Color32(176,176,176,255);
+            theColor.pressedColor = new Color32(176,176,176,255);
+            theColor.pressedColor = new Color32(176,176,176,255);
+            theColor.selectedColor = new Color32(176,176,176,255);
+            theColor.disabledColor = new Color32(255,255,255,100);
+            theColor.colorMultiplier = 1;
+            theColor.fadeDuration = 0.15f;
+            spaceButton.colors = theColor;
 
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
             finalValue = timerValue;
             timerValue = 0;
-
+            theColor.normalColor = new Color32(255,255,255,255);
+            theColor.highlightedColor = new Color32(176,176,176,255);
+            theColor.pressedColor = new Color32(176,176,176,255);
+            theColor.pressedColor = new Color32(176,176,176,255);
+            theColor.selectedColor = new Color32(176,176,176,255);
+            theColor.disabledColor = new Color32(255,255,255,100);
+            theColor.colorMultiplier = 1;
+            theColor.fadeDuration = 0.15f;
+            spaceButton.colors = theColor;
         }
         if (finalValue <= 0.120f && finalValue > 0F)
         {
@@ -66,26 +94,38 @@ public class Timer : MonoBehaviour
             finalValue = 0;
             currentMorse += "-";
             getMorse.text += " — ";
+            
         }
 
 
-
+             spaceDelay -= Time.deltaTime;
             
 
 
         
         if ( hasan.Contains(currentMorse))
         {
-            //getMorse.text = currentMorse;
-            if ( Input.GetKeyDown(KeyCode.E))
-            {
                 int pos = Array.IndexOf(hasan, currentMorse);
                 string number = alphabet[pos];
+            //getMorse.text = currentMorse;
+            if ( Input.GetKeyDown(KeyCode.E))
+                {
+
                 Debug.Log(number);
                 Debug.Log(currentMorse);
-            }
 
-            
+            }
+                
+                if( getWord.text == number)
+                {
+                    getWord.color = new Color32(9,200,9,255);
+                    spaceDelay = 10;
+                }
+                else if(getWord.text != number && spaceDelay < 6)
+                {
+                    getWord.color = new Color32(205,9,9,255);
+                    spaceDelay = 10;
+                }
         }
 
      // int pos = Array.IndexOf(hasan, currentMorse);
@@ -106,6 +146,8 @@ public class Timer : MonoBehaviour
         {
             currentMorse = "";
             getMorse.text = "";
+            spaceDelay = 10;
+
 
         }
 
